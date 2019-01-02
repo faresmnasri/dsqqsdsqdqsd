c0d75e76267bc735415298247dae468e0aba2174
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2019-01-02 13:21:09 +0100

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,16 @@
+﻿const Discord = require('discord.js');
+const cilent = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+cilent.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:rose:  WELCOME TO FALLEGA TEAM SERVER :rose: 
+                               [ https://discord.gg/Vg9EHH2 ] 
+ ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login(process.env.BOT_TOKEN);
